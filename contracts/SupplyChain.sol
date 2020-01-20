7531182d41b7b1b8008a56104e194f44a6eba5d7
carlosescom <carlos.escom.ipn@gmail.com>
2020-01-19 20:46:59 -0600

    + indexed/events

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -52,10 +52,10 @@ contract SupplyChain {
     Prefix each event with "Log" for clarity, so the forSale event will be called "LogForSale"
     Each event should accept one argument, the sku 
   */
-  event LogForSale(uint sku);
-  event LogSold(uint sku);
-  event LogShipped(uint sku);
-  event LogReceived(uint sku);
+  event LogForSale(uint index sku);
+  event LogSold(uint index sku);
+  event LogShipped(uint index sku);
+  event LogReceived(uint index sku);
 
   /* Create a modifer that checks if the msg.sender is the owner of the contract */
 
